04204ec4a3ba7bef6445f983f992575fbeeab837
moramihai <moraru.mihai@ymail.com>
2023-06-13 07:00:21 -0400

    [MONITORING] Added prom endpoint for monitoring

--- src/main/java/ro/unibuc/hello/controller/StudentController.java
+++ src/main/java/ro/unibuc/hello/controller/StudentController.java
@@ -1,5 +1,10 @@
 package ro.unibuc.hello.controller;
 
+
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +18,16 @@ import java.util.List;
 
 public class StudentController {
     private final StudentService studentService;
+    @Autowired
+    MeterRegistry metricsRegistry;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping
+    @Timed(value = "student.fetchAllStudents.time", description = "Time taken to return students")
+    @Counted(value = "student.fetchAllStudents.count", description = "Times students were returned")
     public List<Student> fetchAllStudents(){
         return studentService.getAllStudents();
     }
